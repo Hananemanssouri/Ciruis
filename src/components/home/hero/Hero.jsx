3f926b1781f7c2a7905a514121c7acc43226ea17
Hananemanssouri <hananemanssouri061@gmail.com>
2025-04-29 17:32:18 +0200

    responncive mobile

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -8,8 +8,10 @@ const Hero = () => {
         <section className='hero'>
             <div className="container">
                 <div className="row">
-                  <Title subtitle='Willkommen in der CiRIUS-Center-Welt' title='Online lernen und Zertifikate erwerben' />
-                    <p>Lernen Sie in Ihrem eigenen Tempo und erwerben Sie Online-Zertifizierungen, um Ihre Karriere anzukurbeln. Erweitern Sie Ihre Fähigkeiten mit interaktiven Kursen, auf die Sie überall und jederzeit zugreifen können!</p>
+                  <div className="title1">
+                  <Title subtitle='Willkommen in der CiRIUS-Center-Welt' title='Online lernen und Zertifikate erwerben'  />
+                  </div>
+                    <p className="p11"> Lernen Sie in Ihrem eigenen Tempo und erwerben Sie Online-Zertifizierungen, um Ihre Karriere anzukurbeln. Erweitern Sie Ihre Fähigkeiten mit interaktiven Kursen, auf die Sie überall und jederzeit zugreifen können!</p>
                 <div className="button">
                     <button className='primary-btn'>
                     JETZT STARTEN<i className='fa fa-long-arrow-alt-right'></i>
